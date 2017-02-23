dbcecd6c9ad363c275331336806862bfd55bb2db
Weapon45 <luisorlando.itsx@gmail.com>
2017-02-23 09:34:03 -0600

    rwe
    re

--- js/actions.js
+++ js/actions.js
@@ -89,7 +89,7 @@ var fn = {
 		window.localStorage.setItem("user",'');   
 		$("#txtusuario").val("");
 		$("#txtcontrasena").val("");
-		$("#txtTipo").html('');
+		$('#txtTipo option:first');
 		window.location.href = '#login';
     },	
     ConsultarCUBO: function(){    
