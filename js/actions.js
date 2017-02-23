cc9f2886647265e6cf5b8e041e8315387d4111c1
Weapon45 <luisorlando.itsx@gmail.com>
2017-02-23 09:57:26 -0600

    jmfv
    jvk

--- js/actions.js
+++ js/actions.js
@@ -89,7 +89,7 @@ var fn = {
 		window.localStorage.setItem("user",'');   
 		$("#txtusuario").val("");
 		$("#txtcontrasena").val("");
-		$('#txtTipo').val(1);
+		$('#txtTipo').selectmenu("refresh",true);
 		window.location.href = '#login';
     },	
     ConsultarCUBO: function(){    
